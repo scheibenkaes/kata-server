178eab405833e4820c432ef57538e7c47e07cb9d
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-07-08 19:28:17 +0200

    init

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject kata-server "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :repositories {"stuartsierra" "http://stuartsierra.com/maven2"}
+  :dev-dependencies [[org.clojars.autre/lein-vimclojure "1.0.0"]
+                     [com.stuartsierra/lazytest "1.2.2"]
+                     [lein-autotest "1.1.0"]])
---
+++ test/kata_server/test/core.clj
@@ -0,0 +1,6 @@
+(ns kata-server.test.core
+  (:use [kata-server.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))

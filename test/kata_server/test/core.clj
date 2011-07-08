1674e802371948cd3cba3ea75ad21010369958dc
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-07-08 20:00:56 +0200

    Emptied test file

--- test/kata_server/test/core.clj
+++ test/kata_server/test/core.clj
@@ -1,6 +1,4 @@
-(ns kata-server.test.core
+(ns kata-server.core
   (:use [kata-server.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))

35ddb8ac1f5002dfcff0c2a85676568067e9f586
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-07-08 22:28:20 +0200

    Added server-loop

--- src/kata_server/server.clj
+++ src/kata_server/server.clj
@@ -1,1 +1,17 @@
-(ns kata-server.server)
+(ns kata-server.server
+  (:import [java.net ServerSocket]))
+
+(def *port* 8000)
+
+(defn create-server-socket [] 
+  (ServerSocket. *port*))
+
+(defn server-loop [callback] 
+  (with-open [serv-sock (create-server-socket)]
+    (loop []
+      (let [sock (.accept serv-sock)
+            f (partial callback sock)
+            thread (Thread. f)]
+        (do
+          (.start thread)
+          (recur))))))
--- test/kata_server/test/server.clj
+++ test/kata_server/test/server.clj
@@ -1,1 +1,7 @@
-(ns kata-server.test.server)
+(ns kata-server.test.server
+  (:use kata-server.server)
+  (:use clojure.test))
+
+(deftest create-server-socket-test 
+  (with-open [serv-sock (create-server-socket)]
+    (is (not (nil? serv-sock)))))

8bb905d65a6358a868167e0be1e74cec8babd8ab
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-07-09 12:41:24 +0200

    Added player module

---
+++ src/kata_server/player.clj
@@ -0,0 +1,10 @@
+(ns kata-server.player)
+
+(def allowed-chars #"[a-zA-Z]{3,8}")
+
+(defn legal-name? [s] 
+  (if s 
+    (not (nil? (re-matches allowed-chars s)))
+    false))
+
+(defrecord Player [name])
---
+++ test/kata_server/test/player.clj
@@ -0,0 +1,12 @@
+(ns kata-server.test.player
+  (:use clojure.test)
+  (:use kata-server.player))
+
+(deftest test-legal-name?
+  (are [legal name] (= legal (legal-name? name))
+       true "asdASDF"
+       false "asd4"
+       false "_-aaaa"
+       false ""
+       false "aa"
+       false nil))

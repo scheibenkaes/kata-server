66dc3167f025e898a77cd6e6e144a1bb4538335a
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-07-11 22:51:06 +0200

    Made receive and send multimethods

--- src/kata_server/socket.clj
+++ src/kata_server/socket.clj
@@ -1,17 +1,35 @@
 (ns kata-server.socket
   (:use [clojure.java.io :only [reader writer]]))
 
-(defn receive-line [sock] 
+(defn has-a-socket-in-meta? [obj & _]
+  (-> obj meta :socket nil? not))
+
+(defmulti receive-line has-a-socket-in-meta?)
+
+(defmethod receive-line true
+  [obj]
+  (receive-line (:socket (meta obj))))
+
+(defmethod receive-line false [sock] 
   (when (and sock (.isConnected sock))
     (let [in (reader sock)]
       (.readLine in))))
 
-(defn send-line [sock line] 
+(defmulti send-line has-a-socket-in-meta?)
+
+(defmethod send-line false [sock line]
   (let [out (writer sock)]
     (doto out
       (.write line)
       .newLine
       .flush)))
+
+(defmethod send-line true [player line]
+  (send-line (-> player meta :socket) line))
+
+(defn multicast-line [recipients line] 
+  (doseq [rec recipients]
+    (send-line (:socket rec) line)))
 
 (defn read-line-with-timeout [sock timeout] 
   (let [a (agent nil)

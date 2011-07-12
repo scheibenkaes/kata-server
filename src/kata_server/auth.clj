f65a010dcf857ba38abe49e2d9b765191eaa0271
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-07-12 19:36:06 +0200

    Removed clutter

--- src/kata_server/auth.clj
+++ src/kata_server/auth.clj
@@ -1,13 +1,11 @@
 (ns kata-server.auth
-  (:use [clojure.string :only [split]]
-        [clojure.java.io :only [reader writer]])
   (:require [kata-server.socket :as socket])
   (:use kata-server.player))
 
 (def helo #"HELO (.+)")
 
 (defn authenticate [sock] 
-  (when-let [line (socket/read-line-with-timeout sock 5000)]
+  (when-let [line (socket/receive-line sock)]
     (when-let [cmd (re-find helo (.trim line))]
       (let [n (second cmd)]
-        (with-meta (new-player n) {:socket sock :reader (reader sock) :writer (writer sock)})))))
+        (with-meta (new-player n) {:socket sock})))))

fbf2747d664c16d2a7cd93bb7be4da6e8a9be1cd
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-07-12 18:51:37 +0200

    Added command-line

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject kata-server "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[org.clojars.autre/lein-vimclojure "1.0.0"]
                      [lein-vim "1.0.2-SNAPSHOT"]]
   :main kata-server.core)
--- src/kata_server/core.clj
+++ src/kata_server/core.clj
@@ -1,9 +1,12 @@
 (ns kata-server.core
+  (:use clojure.contrib.command-line)
   (:use kata-server.server)
   (:use kata-server.socket)
   (:use kata-server.auth)
   (:use clojure.java.io)
   (:gen-class))
+
+(def *min-players* 2)
 
 (defn decline-player [sock] 
   (let [out (writer sock)]
@@ -12,16 +15,25 @@
       (.flush out)
       (.close sock))))
 
-(def connected-players (ref #{}))
+(def players-queue (ref []))
 
-(defn add-to-match [player] 
-  (send-line (:socket (meta player)) "HELO "))
+(defn add-to-queue [player] 
+  (do
+    (dosync
+      (alter players-queue conj player))
+    (send-line player (str "HELO " (:name player)))))
 
 (defn on-connection-created [sock] 
   (let [player (authenticate sock)]
     (if player
-      (add-to-match player)
+      (add-to-queue player)
       (decline-player sock))))
 
 (defn -main [& args] 
-  (server-loop on-connection-created))
+  (with-command-line
+    args
+    "Usage java ...-standalone.jar -n number_of_players"
+    [[number n "On how many connected players should a match start" *min-players*]]
+    (do
+      (binding [*min-players* number]
+        (server-loop on-connection-created)))))

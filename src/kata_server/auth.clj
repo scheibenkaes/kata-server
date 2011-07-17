608e52d23851904b01e34a2dcf5bde4b51eea89f
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-07-17 17:32:05 +0200

    README extension
    Also made authenticate check the user name is a re

--- src/kata_server/auth.clj
+++ src/kata_server/auth.clj
@@ -2,10 +2,13 @@
   (:require [kata-server.socket :as socket])
   (:use kata-server.player))
 
+(def name-re #"[\w-_\d ]{2,12}")
+
 (def helo #"HELO (.+)")
 
 (defn authenticate [sock] 
   (when-let [line (socket/receive-line sock)]
     (when-let [cmd (re-find helo (.trim line))]
-      (let [n (second cmd)]
-        (with-meta (new-player n) {:socket sock})))))
+      (let [n (second cmd)
+            checked (re-matches name-re n)]
+        (when checked (with-meta (new-player n) {:socket sock}))))))

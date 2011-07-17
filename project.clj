12cccd021d2efd2cd4d901fc97934661c00b23f7
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-07-17 22:58:11 +0200

    First version of README
    Added warn-on-reflecion to project.clj

--- project.clj
+++ project.clj
@@ -4,4 +4,5 @@
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[org.clojars.autre/lein-vimclojure "1.0.0"]
                      [lein-vim "1.0.2-SNAPSHOT"]]
-  :main kata-server.core)
+  :main kata-server.core
+  :warn-on-reflection true)

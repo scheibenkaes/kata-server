a25c3dc5478029840e631febb9a4f51e108b527b
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-07-18 22:35:31 +0200

    Added highcharts.js

--- src/kata_server/web.clj
+++ src/kata_server/web.clj
@@ -1,1 +1,14 @@
-(ns kata-server.web)
+(ns kata-server.web
+  (:use [noir core])
+  (:use [hiccup core page-helpers]))
+
+(defpartial main-layout [title & body]
+  (html5 
+    [:head
+     [:title title]
+     (include-js "/js/jquery-1.6.2.min.js")
+     (include-js "/js/highcharts.js")]
+    [:body body]))
+
+(defpage "/" []
+  (main-layout "Render"))

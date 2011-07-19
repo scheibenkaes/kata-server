20635af5a9185f8a6e554973610ea6c4f9e39a93
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-07-19 21:40:06 +0200

    Added distribution fns

--- src/kata_server/stats.clj
+++ src/kata_server/stats.clj
@@ -1,6 +1,36 @@
 (ns kata-server.stats
-  (:use clojure.contrib.json))
+  (:use [clojure.contrib.json :only [json-str]]))
 
-(def last-matches (ref []))
+(def last-game-played (ref nil))
+
+(defn valid-throw? [n] (not= 6 n))
+
+(defn add-sums [match] 
+  "Add a map of player name to points to the given match."
+  (assert (contains? match :final-roster))
+  (assoc match :sums (into {} (for [[k v] (:final-roster match)] [k (->> v flatten flatten (apply +))]))))
+
+(defn matches->json [] 
+  (json-str @last-game-played))
+
+(defn add-final-roster [{:keys [roster current-play active-player] :as match}] 
+  "Add the final roster under the :final-roster keyword to the given match."
+  (assoc match :final-roster (update-in roster [active-player] conj current-play)))
+
+(defn player-distri [roster]
+  (let [flat (flatten roster) occ (for [dice (range 1 7)] [dice (count (filter #(= dice %) flat))])]
+    (flatten occ)))
+
+(defn add-throw-distribution [{:keys [final-roster] :as match}]
+  "Adds the distribution of the thrown numbers for each player
+  under :throw-distribution"
+  (assoc match
+         :throw-distribution 
+         (into {} (for [[k v] final-roster] [k (apply hash-map (player-distri (k final-roster)))]))))
 
 
+(defn process-matches [matches] 
+  (dosync
+    (ref-set 
+      last-game-played 
+      (->> (map add-final-roster matches) (map add-sums) (map add-throw-distribution)))))
--- test/kata_server/test/stats.clj
+++ test/kata_server/test/stats.clj
@@ -2,5 +2,34 @@
   (:use clojure.test)
   (:use [kata-server stats]))
 
-(deftest test-last-matches
-  (is (= [] @last-matches)))
+(def test-data 
+  {:roster {:foo [[1 2 3] [4 5]] :bar [[3 2 1]]} :current-play [5 5 5] :active-player :bar})
+
+(def test-game
+  [test-data test-data])
+
+(deftest test-add-final-roster
+  (is (= {:foo [[1 2 3] [4 5]] :bar [[3 2 1] [5 5 5]]}
+         (:final-roster (add-final-roster test-data)))))
+
+(deftest test-add-sums
+  (is (= 15 (-> (add-final-roster test-data) add-sums :sums :foo)))
+  (is (= 21 (-> (add-final-roster test-data) add-sums :sums :bar))))
+
+(deftest test-process-matches
+  (do
+    (dosync (ref-set last-game-played []))
+    (process-matches [test-data])
+    (let [m (first @last-game-played)]
+      (is (contains? m :final-roster))
+      (is (contains? m :sums))
+      (is (contains? m :throw-distribution)))))
+
+(deftest test-add-throw-distri
+  (let [d (add-final-roster test-data)]
+    (is (= {:foo {1 1 2 1 3 1 4 1 5 1 6 0} :bar {1 1 2 1 3 1 4 0 5 3 6 0}}
+           (-> (add-throw-distribution d) :throw-distribution)))))
+
+(deftest test-player-distri 
+  (is (= [1 1 2 1 3 1 4 1 5 1 6 0]
+         (player-distri (-> test-data :roster :foo)))))

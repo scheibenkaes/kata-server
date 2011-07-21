fc07175c8fba5e065bad3ef650e4603df3be4e25
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-07-21 23:37:51 +0200

    Made it v 1.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kata-server "1.1.0-SNAPSHOT"
+(defproject kata-server "1.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]

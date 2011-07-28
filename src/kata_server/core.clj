7b1483de3e24845a528ab3975191109a39c7030f
Benjamin Klüglein <scheibenkaes@googlemail.com>
2011-07-28 11:33:40 +0200

    Changed web server port
    * Is now (inc port) and not a constant
    * Bumped version

--- src/kata_server/core.clj
+++ src/kata_server/core.clj
@@ -75,5 +75,5 @@
       (reset! *min-players* (Integer/parseInt number))
       (reset! *max-points* (Integer/parseInt points))
       (reset! *rounds-to-play* (Integer/parseInt matches))
-      (when web? (noir/start 8001 {}))
+      (when web? (noir/start (inc (Integer/parseInt port)) {}))
       (server-loop on-connection-created :port (Integer/parseInt port)))))
